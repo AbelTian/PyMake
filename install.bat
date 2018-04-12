70d200716d7545f91ca15ace4f3374fea0c930e8
tianduanrui <tianduanrui@163.com>
2018-04-12 19:02:49 +0800

    update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @echo off
 set filepath=%~dp0
 set mmfile=C:\Windows\mm.bat
-echo @echo off > %mmfile%
-echo call "%filepath%pymake.bat" %* >> %mmfile%
+echo @echo off > %mmfile% 
+echo call "%filepath%pymake.bat" %* >> %mmfile% 
+rem 请使用 install.bat %* 进行安装
 
