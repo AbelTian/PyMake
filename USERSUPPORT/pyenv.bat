f1d8a9ba92816a47f731b8624611194fad4d184f
tianduanrui <tianduanrui@163.com>
2019-04-21 11:51:52 +0800

    add usersupport pyinstall.bat

--- USERSUPPORT/pyenv.bat
+++ USERSUPPORT/pyenv.bat
@@ -3,23 +3,23 @@
 ::本文件变量，局部有效，不开启这个设置。
 ::setlocal enabledelayedexpansion
 
-set PYWORKFLAG=1
+set PYENVFLAG=1
 if "%1" == "" (
     echo please appoint a env name. & exit /b 0
 ) else if "%1" == "open" (
     if "%2" == "" ( echo please appoint a env name. & exit /b 0 )
-    set PYUSERENVNAME=%2
+    set PYENVNAME=%2
 ) else if "%1" == "close" (
     if "%2" == "" ( echo please appoint a env name. & exit /b 0 )
-    set PYUSERENVNAME=%2
-    set PYWORKFLAG=0
+    set PYENVNAME=%2
+    set PYENVFLAG=0
 ) else (
-    set PYUSERENVNAME=%1
+    set PYENVNAME=%1
 )
 
-set PYPROGRAMPATH=%~dp0
-set PYPROGRAMNAME=..\pymake.bat
-set PYPROGRAMPATHNAME=%PYPROGRAMPATH%%PYPROGRAMNAME%
+set PYPROGRAMPATH=%~dp0..
+set PYPROGRAMNAME=pymake.bat
+set PYPROGRAMPATHNAME=%PYPROGRAMPATH%\%PYPROGRAMNAME%
 
 echo preparing env ...
 for /F %%i in ('echo %random%') do ( set "PYENVINDEX=%%i" )
@@ -29,14 +29,14 @@ echo location : [%PYMMSOURCEROOT%]
 for /F %%i in ('"%PYPROGRAMPATHNAME%" source config') do ( set "PYMMSOURCECONFIG=%%i" )
 echo configure: [%PYMMSOURCECONFIG%] [1]
 
-call "%PYPROGRAMPATHNAME%" export %PYUSERENVNAME% %PYENVINDEX%
+call "%PYPROGRAMPATHNAME%" export %PYENVNAME% %PYENVINDEX%
 
-if %PYWORKFLAG% == 0 (
+if %PYENVFLAG% == 0 (
     call "%PYMMSOURCEROOT%\%PYENVINDEX%_unset.bat"
-    echo user env : [%PYUSERENVNAME%] closed
+    echo user env : [%PYENVNAME%] closed
 ) else (
     call "%PYMMSOURCEROOT%\%PYENVINDEX%_effect.bat"
-    echo user env : [%PYUSERENVNAME%] opend
+    echo user env : [%PYENVNAME%] opend
 )
 
 ::clean
---
+++ USERSUPPORT/pyuninstall.bat
@@ -0,0 +1,5 @@
+@echo off
+set mmfile=C:\Windows\pyenv.bat
+del /q /f %mmfile% 
+
+
