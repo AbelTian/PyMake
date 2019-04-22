72fabeeecac55607bcdb91a89d587cb1a290c1c8
tianduanrui <tianduanrui@163.com>
2019-04-22 11:03:18 +0800

    fix pyvc.bat

--- USERSUPPORT/pyvc.bat
+++ USERSUPPORT/pyvc.bat
@@ -5,10 +5,10 @@ call %PYFILEPATH%\pyenv.bat %*
 
 if %PYENVFLAG% == 0 (
     echo pyvc env : CLS-VCVARSALL="%CLS-VCVARSALL:/=\%"
-    call "%CLS-VCVARSALL:/=\%" "%CLS-VCVARSALLPARAM%"
+    call "%CLS-VCVARSALL:/=\%" %CLS-VCVARSALLPARAM%
     echo pyvc env : [%PYENVNAME%] closed, but you should use new vcvarsall overlap it.
 ) else (
     echo pyvc env : VCVARSALL="%VCVARSALL:/=\%" "%VCVARSALLPARAM%"
-    call "%VCVARSALL:/=\%" "%VCVARSALLPARAM%"
+    call "%VCVARSALL:/=\%" %VCVARSALLPARAM%
     echo pyvc env : [%PYENVNAME%] opened, sure.
 )
