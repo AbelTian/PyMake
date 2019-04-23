9c84d5d60656dc9d61a3f0a1dc14d15c38ba4d72
tianduanrui <tianduanrui@163.com>
2019-04-23 16:51:43 +0800

    更新pycmd.bat

--- USERSUPPORT/pycmd.bat
+++ USERSUPPORT/pycmd.bat
@@ -1,25 +1,24 @@
 @echo off
 
-:: pycmd.bat 会强制使用pymake.bat的默认环境配置。即自带一个默认环境。
-:: pycmd.bat 会使用默认环境配置导出命令，
-:: pycmd.bat 却没有导出默认环境使用。
-:: pycmd.bat 所以用户必须谨慎使用，以防导出的不是自己想要的命令，或者命令无法执行。
+:: pycmd.bat 在当前环境执行pymake保存的命令，自带一个环境。
+:: pycmd.bat 使用pymake的默认环境导出命令，如果用户输入环境，那么使用用户输入的环境。
+:: pycmd.bat 会导出环境进行使用。
+:: pycmd.bat 用户应当留意，导出的是否是目标环境的命令。
 
 if "%1" == "" (
+    echo "pycmd <cmd-name> [ <env-name> ]"
     echo please appoint a cmd name. & exit /b 0
-) else if "%1" == "exec" (
-    if "%2" == "" ( echo please appoint a env name. & exit /b 0 )
-    set PYEXECNAME=%2
-) else if "%1" == "cc" (
-    if "%2" == "" ( echo please appoint a env name. & exit /b 0 )
-    set PYEXECNAME=%2
-) else (
-    set PYEXECNAME=%1
+)
+set PYEXECNAME=%1
+
+if not "%2" == "" (
+    set PYENVNAME=%2
 )
 
 set PYPROGRAMPATH=%~dp0..
 set PYPROGRAMNAME=pymake.bat
 set PYPROGRAMPATHNAME=%PYPROGRAMPATH%\%PYPROGRAMNAME%
+
 
 echo starting cmd ...
 for /F %%i in ('echo %random%') do ( set "PYEXECINDEX=%%i" )
@@ -28,13 +27,26 @@ for /F %%i in ('"%PYPROGRAMPATHNAME%" source root') do ( set "PYMMSOURCEROOT=%%i
 echo location : [%PYMMSOURCEROOT%]
 for /F %%i in ('"%PYPROGRAMPATHNAME%" source config') do ( set "PYMMSOURCECONFIG=%%i" )
 echo configure: [%PYMMSOURCECONFIG%] [1]
+
+
 for /F %%i in ('"%PYPROGRAMPATHNAME%" get current env') do ( set "PYENVNAME=%%i" )
 echo environme: [%PYENVNAME%] [default]
-for /F %%i in ('"%PYPROGRAMPATHNAME%" have cmd %PYEXECNAME%') do ( set "PYEXECFLAG=%%i" )
-if "%PYEXECFLAG%" == 0 (
-    echo pycmd env: [%PYEXECNAME%] is not existed. exit 1.
-) else (
-    call "%PYPROGRAMPATHNAME%" type here %PYEXECNAME% %PYEXECINDEX%
-    call "%CD%\%PYEXECINDEX%_exec.bat"
-    del /q /f "%CD%\%PYEXECINDEX%_exec.bat"
+if not "%2" == "" (
+    set PYENVNAME=%2
 )
+for /F %%i in ('"%PYPROGRAMPATHNAME%" have env %PYENVNAME%') do ( set "PYEXECFLAG=%%i" )
+if "%PYEXECFLAG%" == "False" (
+    echo environme: [%PYENVNAME%] is not existed.
+    exit /b 0
+)
+echo environme: [%PYENVNAME%] [%PYEXECFLAG%] [USED]
+
+for /F %%i in ('"%PYPROGRAMPATHNAME%" have cmd %PYEXECNAME%') do ( set "PYEXECFLAG=%%i" )
+echo command  : [%PYEXECNAME%] [%PYEXECFLAG%]
+
+call "%PYPROGRAMPATHNAME%" export here %PYENVNAME% to %PYEXECINDEX%
+call "%PYPROGRAMPATHNAME%" use %PYENVNAME% type here %PYEXECNAME% to %PYEXECINDEX%
+call "%CD%\%PYEXECINDEX%_effect.bat"
+call "%CD%\%PYEXECINDEX%_exec.bat"
+del /q /f "%CD%\%PYEXECINDEX%_exec.bat"
+del /q /f "%CD%\%PYEXECINDEX%_effect.bat" "%CD%\%PYEXECINDEX%_unset.bat"
