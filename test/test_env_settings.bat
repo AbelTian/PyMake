9f6225cd8670ed464603a9c83930dd80a0440b63
tianduanrui <tianduanrui@163.com>
2019-06-13 10:16:17 +0800

    update testcase.

---
+++ test/test_env_settings.bat
@@ -0,0 +1,55 @@
+@echo off
+
+for /F %%i in ('"pymake get default exec root"') do ( set "WORKROOT=%%i" )
+set WORKROOT=%WORKROOT%\MyShell
+set WORKROOT=%WORKROOT:/=\\%
+md %WORKROOT%
+
+set command=mm use current ccvp "which lua"
+echo %command%
+call %command%
+
+set command=mm use current ccvp "which go"
+echo %command%
+call %command%
+
+rem set command=echo cmake & mm use current ccvp which cmake
+set command=mm use current ccvp which cmake
+echo %command%
+call %command%
+
+
+set command=mm use current ccvp which perl
+echo %command%
+call %command%
+
+set command=mm use current ccvp which php
+echo %command%
+call %command%
+
+set command=mm use current ccvp which ruby
+echo %command%
+call %command%
+
+set command=mm use current ccvp which r
+echo %command%
+call %command%
+
+set command=mm use current ccvp which node
+echo %command%
+call %command%
+
+set command=mm use current ccvp which elixir
+echo %command%
+call %command%
+
+set cmdname=qmake
+call :find_exec
+
+exit /b 0
+
+:find_exec
+set command=mm use current ccvp which %cmdname%
+echo %command%
+call %command%
+goto :eof
