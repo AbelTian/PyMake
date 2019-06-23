729aca555d53fecbddc291f9142bf06fc2fe8167
tianduanrui <tianduanrui@163.com>
2019-06-23 11:53:24 +0800

    v7.7.1

--- pymake7.py
+++ pymake7.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-"""PyMake 7.7.
+"""PyMake 7.7.1.
 
 Usage:
   pymake7.py  source
@@ -844,7 +844,7 @@ def main_function():
         conf.set('source', 'config', pymakedefaultsourcefile)
         conf.write(open(pymakeini, 'w'))
 
-    args = docopt(__doc__, version='pymake7.py v7.7')
+    args = docopt(__doc__, version='pymake7.py v7.7.1')
     #print(args)
 
     #initialize
