d5d0f8df2c066a52ae4acef780eabf51ebe07e78
tianduanrui <2407223896@qq.com>
2020-04-29 11:22:17 +0800

    更改pycmd.sh的功能，和pymake cmd表现一致。

--- pycmd.bat
+++ pycmd.bat
@@ -1,7 +1,45 @@
 @echo off
 
+:: pyclean.bat 在当前目录，清理掉所有导出的命令脚本。
+
 set PYPROGRAMPATH=%~dp0
-set PYPROGRAMNAME=pyexecvp.bat
+set PYPROGRAMNAME=pymake.bat
 set PYPROGRAMPATHNAME=%PYPROGRAMPATH%%PYPROGRAMNAME%
 
-call "%PYPROGRAMPATHNAME%" %*
+
+rem echo starting cmd ...
+rem for /F %%i in ('echo %random%') do ( set "PYEXECINDEX=%%i" )
+rem echo cmd index: [%PYEXECINDEX%]
+rem for /F %%i in ('"%PYPROGRAMPATHNAME%" source root') do ( set "PYMMSOURCEROOT=%%i" )
+rem echo location : [%PYMMSOURCEROOT%]
+rem for /F %%i in ('"%PYPROGRAMPATHNAME%" source config') do ( set "PYMMSOURCECONFIG=%%i" )
+rem echo configure: [%PYMMSOURCECONFIG%] [1]
+
+rem for /F %%i in ('"%PYPROGRAMPATHNAME%" get current env') do ( set "PYMMDEFAULTENVNAME=%%i" )
+rem echo environme: [%PYMMDEFAULTENVNAME%] [default]
+rem if not "%2" == "" (
+rem     set PYENVNAME=%2
+rem ) else (
+rem     set PYENVNAME=%PYMMDEFAULTENVNAME%
+rem )
+
+rem for /F %%i in ('"%PYPROGRAMPATHNAME%" have env %PYENVNAME%') do ( set "PYEXECFLAG=%%i" )
+rem if "%PYEXECFLAG%" == "False" (
+rem     echo environme: [%PYENVNAME%] is not existed.
+rem     exit /b 0
+rem )
+rem echo environme: [%PYENVNAME%] [%PYEXECFLAG%] [USED]
+
+rem for /F %%i in ('"%PYPROGRAMPATHNAME%" have cmd %PYEXECNAME%') do ( set "PYEXECFLAG=%%i" )
+rem if "%PYEXECFLAG%" == "False" (
+rem     echo command  : [%PYEXECNAME%] is system wild command.
+rem ) else (
+rem     echo command  : [%PYEXECNAME%] [%PYEXECFLAG%] [EXISTED]
+rem )
+
+rem for /F %%i in ('"%PYPROGRAMPATHNAME%" get default exec root') do ( set "PYMMSHELLROOT=%%i" )
+rem echo exec root: [%PYMMSHELLROOT%] [default]
+rem echo exec root: [%CD%] [here]
+
+call "%PYPROGRAMPATHNAME%" cmd %*
+
