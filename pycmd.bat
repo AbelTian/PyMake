83ce19d106af639f5bd1d563a340f5150d0b245a
tianduanrui <tianduanrui@163.com>
2020-04-29 11:04:36 +0800

    update pycmd.bat and pycmd.sh

--- pycmd.bat
+++ pycmd.bat
@@ -1,54 +1,7 @@
 @echo off
 
-:: pycmd.bat 使用某个环境，在当前目录，执行命令。
-
-if ""%1"" == """" (
-    rem echo you can execute command with params in default env.
-    echo usage:
-    echo "  pycmd <cmd-name>"
-    echo ------
-    echo please appoint a cmd name. & exit /b 0
-)
-set PYEXECNAME=%1
-
 set PYPROGRAMPATH=%~dp0
-set PYPROGRAMNAME=pymake.bat
+set PYPROGRAMNAME=pyexecvp.bat
 set PYPROGRAMPATHNAME=%PYPROGRAMPATH%%PYPROGRAMNAME%
 
-
-rem echo starting cmd ...
-rem for /F %%i in ('echo %random%') do ( set "PYEXECINDEX=%%i" )
-rem echo cmd index: [%PYEXECINDEX%]
-rem for /F %%i in ('"%PYPROGRAMPATHNAME%" source root') do ( set "PYMMSOURCEROOT=%%i" )
-rem echo location : [%PYMMSOURCEROOT%]
-rem for /F %%i in ('"%PYPROGRAMPATHNAME%" source config') do ( set "PYMMSOURCECONFIG=%%i" )
-rem echo configure: [%PYMMSOURCECONFIG%] [1]
-
-rem for /F %%i in ('"%PYPROGRAMPATHNAME%" get current env') do ( set "PYMMDEFAULTENVNAME=%%i" )
-rem echo environme: [%PYMMDEFAULTENVNAME%] [default]
-rem if not "%2" == "" (
-rem     set PYENVNAME=%2
-rem ) else (
-rem     set PYENVNAME=%PYMMDEFAULTENVNAME%
-rem )
-set PYENVNAME=current
-
-rem for /F %%i in ('"%PYPROGRAMPATHNAME%" have env %PYENVNAME%') do ( set "PYEXECFLAG=%%i" )
-rem if "%PYEXECFLAG%" == "False" (
-rem     echo environme: [%PYENVNAME%] is not existed.
-rem     exit /b 0
-rem )
-rem echo environme: [%PYENVNAME%] [%PYEXECFLAG%] [USED]
-
-rem for /F %%i in ('"%PYPROGRAMPATHNAME%" have cmd %PYEXECNAME%') do ( set "PYEXECFLAG=%%i" )
-rem if "%PYEXECFLAG%" == "False" (
-rem     echo command  : [%PYEXECNAME%] is system wild command.
-rem ) else (
-rem     echo command  : [%PYEXECNAME%] [%PYEXECFLAG%] [EXISTED]
-rem )
-
-rem for /F %%i in ('"%PYPROGRAMPATHNAME%" get default exec root') do ( set "PYMMSHELLROOT=%%i" )
-rem echo exec root: [%PYMMSHELLROOT%] [default]
-rem echo exec root: [%CD%] [here]
-
-call "%PYPROGRAMPATHNAME%" use %PYENVNAME% exec here %PYEXECNAME%
+call "%PYPROGRAMPATHNAME%" %*
--- pycmd.sh
+++ pycmd.sh
@@ -1,46 +1,8 @@
 #!/usr/bin/env bash
 
-#main while
 while [ 1 ]
 do
 
-
-if [ "$1" = "" ]; then
-    echo usage:
-    echo "  pycmd <cmd-name>"
-    echo ------
-    echo please appoint a cmd name.
-    break
-fi
-export PYEXECNAME=$1
-
-
-#if has source[.] call , failed. source default work path is user home.
-#这些都只是获取到了工作路径
-#used for a link to sh
-#file=$(readlink -n "$0")
-#export PYPROGRAMPATH=${file%/*}
-#if [ "${PYPROGRAMPATH}" != "" ]; then
-#    PYPROGRAMPATH=$PYPROGRAMPATH/..
-#else
-#    #used for a real call to sh
-#    PYPROGRAMPATH=$(cd `dirname $0`; pwd)/..
-#fi
-
-#used for call directory to this sh, include source[.] call.
-#export PYPROGRAMPATH=$(cd ..; pwd)
-
-#这个有价值，这个可以获得 source 直接调用的文件名。但是还没有达到目的。source调用的必须找到真的文件。
-#DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-#echo "dir_path is $DIR_PATH"
-
-#这段代码彻底解决了问题，怎么调用都是对的。= __file__
-#在任意目录，
-#直接调用 sh 文件
-#调用 link
-#source 调用 sh 文件
-#source 调用 link 文件
-#结果都是准确的。
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
@@ -51,55 +13,10 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
 #echo .... $DIR
 
 export PYPROGRAMPATH=${DIR}
-export PYPROGRAMNAME=pymake.sh
+export PYPROGRAMNAME=pyexecvp.sh
 export PYPROGRAMPATHNAME=$PYPROGRAMPATH/$PYPROGRAMNAME
 
-#echo $file
-#echo $PYPROGRAMPATH
-#echo $(pwd)
-##. 获取到的是 -bash
-#echo "$0" "$1"
-#echo $(filename "$0")
-#echo $0 | awk -F'\' '{print $NF}'
-
-#echo starting cmd ...
-#export PYEXECINDEX=$(echo $RANDOM)
-#echo env index: \[$PYEXECINDEX\]
-
-#export PYMMSOURCEROOT=$("${PYPROGRAMPATHNAME}" source root)
-#echo location : \[$PYMMSOURCEROOT\]
-
-#export PYMMSOURCECONFIG=$("$PYPROGRAMPATHNAME" source config)
-#echo configure: \[$PYMMSOURCECONFIG\] \[1\]
-
-#export PYMMDEFAULTENVNAME=$("$PYPROGRAMPATHNAME" get current env)
-#echo environme: \[$PYMMDEFAULTENVNAME\] \[default\]
-#if [ "$2" != "" ]; then
-#    export PYENVNAME=$2
-#else
-#    export PYENVNAME=$PYMMDEFAULTENVNAME
-#fi
-export PYENVNAME=current
-
-#export PYEXECFLAG=$("$PYPROGRAMPATHNAME" have env $PYENVNAME)
-#if [ "$PYEXECFLAG" = "False" ]; then
-#    echo environme: \[$PYENVNAME\] is not existed.
-#    break
-#fi
-#echo environme: \[$PYENVNAME\] \[$PYEXECFLAG\] \[USED\]
-
-#export PYEXECFLAG=$("$PYPROGRAMPATHNAME" have cmd $PYEXECNAME)
-#if [ "$PYEXECFLAG" = "False" ]; then
-#    echo command\ \ : \[$PYEXECNAME\] is system wild command.
-#else
-#    echo command\ \ : \[$PYEXECNAME\] \[$PYEXECFLAG\] \[EXISTED\]
-#fi
-
-#export PYMMSHELLROOT=$("$PYPROGRAMPATHNAME" get default exec root)
-#echo exec root: \[$PYMMSHELLROOT\] \[default\]
-#echo exec root: \[$(pwd)\] \[here\]
-
-"$PYPROGRAMPATHNAME" use $PYENVNAME exec here $PYEXECNAME
+"$PYPROGRAMPATHNAME" "$@"
 exit $?
 
 break
