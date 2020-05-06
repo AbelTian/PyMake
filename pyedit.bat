da77d223db5261c7af575711f125d8c27f07be86
tianduanrui <tianduanrui@163.com>
2020-05-06 11:10:56 +0800

    添加pyedit.exe

--- pyedit.bat
+++ pyedit.bat
@@ -1,7 +1,12 @@
 @echo off
 
+rem set PYPROGRAMPATH=%~dp0
+rem set PYPROGRAMNAME=tools\pyedit\pyedit.py
+rem set PYPROGRAMPATHNAME=%PYPROGRAMPATH%%PYPROGRAMNAME%
+rem call py "%PYPROGRAMPATHNAME%" %*
+
 set PYPROGRAMPATH=%~dp0
-set PYPROGRAMNAME=tools\pyedit\pyedit.py
+set PYPROGRAMNAME=tools\pyedit\pyedit.exe
 set PYPROGRAMPATHNAME=%PYPROGRAMPATH%%PYPROGRAMNAME%
 
-call py "%PYPROGRAMPATHNAME%" %*
+start "" "%PYPROGRAMPATHNAME%" %*
